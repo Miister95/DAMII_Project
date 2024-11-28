60c9942ab98db5f3354aee030601cc40a6e16fe3
Miister95 <cairne_abraham@hotmail.com>
2024-11-28 08:27:27 -0500

    Project base

---
+++ Project_E-commerce-NikeTests/Project_E_commerce_NikeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Project_E_commerce_NikeTests.swift
+//  Project_E-commerce-NikeTests
+//
+//  Created by Miister on 28/11/24.
+//
+
+import Testing
+@testable import Project_E_commerce_Nike
+
+struct Project_E_commerce_NikeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
