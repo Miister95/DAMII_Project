a3847e57529ee4f4627da9ef07f82269201c4f7e
Miister95 <cairne_abraham@hotmail.com>
2024-12-01 10:33:50 -0500

    Four welcome views

--- Project_E-commerce-Nike/ViewControllers/ViewController.swift
+++ Project_E-commerce-Nike/ViewControllers/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var nikeLogoImageView: UIImageView!
+    
+    override func viewDidAppear(_ animated:Bool) {
+        super.viewWillAppear(animated)
+        animateLogo()
     }
-
-
+    
+    private func animateLogo() {
+        // Anima el logo reduciendo su escala y desvaneciéndolo
+        UIView.animate(withDuration: 2.0, animations: {
+            self.nikeLogoImageView.transform = CGAffineTransform(scaleX: 1.5, y: 1.5) // Escala el logo
+            self.nikeLogoImageView.alpha = 0.0 // Desvanece el logo
+        }) { _ in
+            // Presenta el siguiente ViewController al terminar la animación
+            self.presentNextScreen()
+        }
+    }
+    
+    private func presentNextScreen() {
+        // Identificador del siguiente ViewController en tu Storyboard
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let welcome1ViewController = storyboard.instantiateViewController(withIdentifier: "Welcome1ViewController")
+        
+        // Configuración de la transición
+        welcome1ViewController.modalTransitionStyle = .crossDissolve
+        welcome1ViewController.modalPresentationStyle = .fullScreen
+        
+        // Presentar el siguiente ViewController
+        self.present(welcome1ViewController, animated: true, completion: nil)
+    }
 }
 
---
+++ Project_E-commerce-Nike/ViewControllers/Welcome1ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Welcome1ViewController.swift
+//  Project_E-commerce-Nike
+//
+//  Created by Miister on 30/11/24.
+//
+
+import UIKit
+
+class Welcome1ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
