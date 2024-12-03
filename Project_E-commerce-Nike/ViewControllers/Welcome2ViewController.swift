c19ad2428044202aa8d376442be6a66e18bfd388
Miister95 <cairne_abraham@hotmail.com>
2024-12-03 10:08:16 -0500

    Four views + view animated

---
+++ Project_E-commerce-Nike/ViewControllers/Welcome2ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  Welcome2ViewController.swift
+//  Project_E-commerce-Nike
+//
+//  Created by Miister on 30/11/24.
+//
+
+import UIKit
+
+class Welcome2ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var img1: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+
+}
